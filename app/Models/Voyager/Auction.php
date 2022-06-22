e18d2abf95b6ad7195659116acfad08f4ff5fadd
Julians22 <dabnerjulian@gmail.com>
2022-06-22 21:26:01 +0700

    update

--- app/Models/Voyager/Auction.php
+++ app/Models/Voyager/Auction.php
@@ -4,8 +4,11 @@ namespace App\Models\Voyager;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Auction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 }
